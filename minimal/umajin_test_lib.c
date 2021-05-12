a2bfcbd5cb5b3a3ec987b6cc76095e264bcb2c46
Kuzma Shapran <kuzma.shapran@umajin.com>
2021-05-13 09:43:49 +1200

    Add license and comments, and format

--- minimal/umajin_test_lib.c
+++ minimal/umajin_test_lib.c
@@ -1,38 +1,44 @@
-//
-//  umajin_test_lib.c
-//  UmajinTestLib
-//
-//  Created by Aaron Lawrence on 29/04/21.
-//
+/*
+ *
+ *  Project:      Umajin External Library Example
+ *  Author:       Aaron Lawrence <Aaron.Lawrence@umajin.com>
+ *                Kuzma Shapran <Kuzma.Shapran@umajin.com>
+ *  License:      CC0
+ *
+ * To the extent possible under law, Umajin has waived all copyright and related or neighboring
+ * rights to Umajin External Library Example.
+ *
+ */
 
 #include "umajin_test_lib.h"
+
 #include <string.h>
 
 int replyToPoll = 0;
 
 const char* getIdentifier()
 {
-   return "UmajinTestLib";
+	return "UmajinTestLib";
 }
 
 const char* process(long long tag, const char* payload)
 {
-   if ( strcmp( payload, "action" ) == 0 )
-   {
-      replyToPoll = 1;
-   }
-   return "UmajinTestLib process says: maybe";
+	if (strcmp(payload, "action") == 0)
+	{
+		replyToPoll = 1;
+	}
+	return "UmajinTestLib process says: maybe";
 }
 
 const char* poll(long long tag, long long timestamp)
 {
-   if ( replyToPoll )
-   {
-      replyToPoll = 0;
-      return "UmajinTestLib says hello in response";
-   }
-   else
-   {
-      return "";
-   }
+	if (replyToPoll)
+	{
+		replyToPoll = 0;
+		return "UmajinTestLib says hello in response";
+	}
+	else
+	{
+		return "";
+	}
 }
--- minimal/umajin_test_lib.h
+++ minimal/umajin_test_lib.h
@@ -1,27 +1,38 @@
-//
-//  umajin_test_lib.h
-//  UmajinTestLib
-//
-//  Created by Aaron Lawrence on 29/04/21.
-//
+/*
+ *
+ *  Project:      Umajin External Library Example
+ *  Author:       Aaron Lawrence <Aaron.Lawrence@umajin.com>
+ *                Kuzma Shapran <Kuzma.Shapran@umajin.com>
+ *  License:      CC0
+ *
+ * To the extent possible under law, Umajin has waived all copyright and related or neighboring
+ * rights to Umajin External Library Example.
+ *
+ */
 
-#ifndef umajin_test_lib_h
-#define umajin_test_lib_h
+#ifndef UMAJIN_TEST_LIB_H
+#define UMAJIN_TEST_LIB_H
 
-#ifdef _MSC_VER 
-#define EXPORT __declspec(dllexport)
+#include <stdint.h>
+
+#if defined(_WIN32) || defined(_WIN64) // Windows:
+	#define EXPORT __declspec(dllexport)
+#else // *nix:
+	#define EXPORT __attribute__((visibility("default")))
 #endif
-#ifdef __APPLE__
-#define EXPORT 
-#endif
 
-// TBD: conflict between function "poll" and standard includes!
-// #include <stdio.h>
+// All functions are guaranteed to be called within the same thread, so no multithread safety is
+// required in the library.
 
-EXPORT const char* getIdentifier(void);
+// The returned identifier has to be GLOBALLY unique between all external libraries and also it
+// has to be an immutable value - i.e. the value cannot change between calls.
+EXPORT const char* getIdentifier();
 
-EXPORT const char* process(long long tag, const char* payload);
+EXPORT const char* process(
+    uint64_t tag /* Unique identifier for the customer. It can be used as a key to store state. */,
+    const char* payload);
 
-EXPORT const char* poll(long long tag, long long timestamp);
+EXPORT const char* poll(uint64_t tag /* The same as in process(). */,
+    uint64_t timestamp /* Microseconds since app launch. */);
 
-#endif /* umajin_test_lib_h */
+#endif // UMAJIN_TEST_LIB_H
